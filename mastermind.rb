bd7dd5631a4d025873816aee77d437e3c64d4308
sipandrei <pontos.serban@gmail.com>
2021-04-22 10:39:15 +0300

    number cleaning and bug fixes

--- mastermind.rb
+++ mastermind.rb
@@ -1,45 +1,13 @@
-# method for spliting numbers
-module NumberSpliter
-  @possible_digit = [1, 2, 3, 4, 5, 6]
-
-  private
-
-  def split_number(number)
-    number.split('')
-    number.each(&:to_i)
-  end
-
-  def number_cleaning(number)
-    if number.instance_of?(Array)
-      if number.length > 4
-        number.pop(number.length - 4)
-      elsif number.length < 4
-        (number.length - 4).times(number.push(@possible_digit.sample))
-      end
-      number.each{ |digit| digit = 6 unless digit.between?(1, 6)}
-    else
-      puts 'You need to pass an array'
-    end
-  end
-end
-
 # Code maker
 class CodeMaker
-  include NumberSpliter
-
-  @notice = "Give a four-digit number with each digit smaller than 6 \n
-            NOTE:if given a longer number, I use only the first four digits,digit greater than 6 becomes 6"
   def initialize(type)
     case type
     when type == true
-      puts @@NOTICE
-      @number = gets.chomp
-      split_number(@number)
-      number_cleaning(@number)
+      puts 'Give a four-digit number with each digit smaller than 6'
+      @number = gets.chomp.split('').map { |digit| digit.to_i }
+      number_cleaning
     when type == false
       make_number
-    else
-      puts 'type needs to be player or computer'
     end
   end
 
@@ -51,11 +19,24 @@ class CodeMaker
       @number.push(@possible_digit.sample)
     end
   end
+
+  def number_cleaning
+    if @number.instance_of?(Array)
+      if @number.length > 4
+        @number.pop(@number.length - 4)
+      elsif @number.length < 4
+        (4 - @number.length).times { @number.push((Array 1..6).sample) }
+      end
+      @number.each_with_index { |digit, index| @number[index] = 6 unless digit.between?(1, 6) }
+      p @number
+    else
+      puts 'You need to pass an array'
+    end
+  end
 end
 
 # Code breaker
 class CodeBreaker
-  include NumberSpliter
   @turns = 12
   def initialize(type)
     @type = type
@@ -64,3 +45,5 @@ class CodeBreaker
   def play_game
   end
 end
+
+CodeMaker.new(true)
